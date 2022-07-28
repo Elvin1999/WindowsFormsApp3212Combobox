7acf0523ecdd49f8b90c636a41fe158a1a6f1aeb
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-07-28 11:55:17 +0400

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bookListbox = new System.Windows.Forms.ListBox();
+            this.genreCombox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(39, 38);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(299, 316);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(370, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(39, 378);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(242, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteBtn.Location = new System.Drawing.Point(361, 299);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(245, 55);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete All Done";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(361, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 33);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // bookListbox
+            // 
+            this.bookListbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bookListbox.FormattingEnabled = true;
+            this.bookListbox.ItemHeight = 31;
+            this.bookListbox.Location = new System.Drawing.Point(724, 40);
+            this.bookListbox.Name = "bookListbox";
+            this.bookListbox.Size = new System.Drawing.Size(327, 314);
+            this.bookListbox.TabIndex = 6;
+            // 
+            // genreCombox
+            // 
+            this.genreCombox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.genreCombox.FormattingEnabled = true;
+            this.genreCombox.Location = new System.Drawing.Point(1090, 40);
+            this.genreCombox.Name = "genreCombox";
+            this.genreCombox.Size = new System.Drawing.Size(148, 33);
+            this.genreCombox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1452, 567);
+            this.Controls.Add(this.genreCombox);
+            this.Controls.Add(this.bookListbox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox bookListbox;
+        private System.Windows.Forms.ComboBox genreCombox;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Author}  -  {Title}  -  {Genre}";
+        }
+    }
+    public partial class Form1 : Form
+    {
+        List<Book> books;
+        public Form1()
+        {
+            InitializeComponent();
+            books = new List<Book>
+            {
+                new Book
+                {
+                    Author="John",
+                    Title="Book 1",
+                    Genre="Dram"
+                },
+                new Book
+                {
+                    Author="John",
+                    Title="Book 2",
+                    Genre="Dram"
+                },
+                new Book
+                {
+                    Author="Leyla",
+                    Title="Book 3",
+                    Genre="Comedy"
+                },
+                    new Book{
+                    Author="John",
+                    Title="Book 4",
+                    Genre="Sci-fi"
+
+                },
+                    new Book{
+                    Author="Akif",
+                    Title="Book 5",
+                    Genre="Sci-fi"
+
+                },
+            };
+
+
+            bookListbox.DataSource = books;
+
+            var genres = books.Select(b => b.Genre).ToList().Distinct();
+            genreCombox.Items.Add("All");
+            genreCombox.Items.AddRange(genres.ToArray());
+
+            genreCombox.SelectedIndex = 0;
+
+            genreCombox.SelectedIndexChanged += GenreCombox_SelectedIndexChanged;
+
+            //Book Title Author Genre
+
+            //Books
+
+
+
+
+
+            List<string> todos = new List<string>
+            {
+                "Read Book",
+                "Drink Coffee",
+                "Write Code",
+                "Write more code",
+                "Play Tennis"
+            };
+
+            //todos.Select(x => x.Trim()).ToList().Distinct();
+
+            checkedListBox1.Items.AddRange(todos.ToArray());
+
+            comboBox1.DataSource = todos;
+
+            this.BackColor = Color.White;
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void GenreCombox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var currentGenre = genreCombox.SelectedItem as string;
+            if (currentGenre == "All")
+            {
+                bookListbox.DataSource = books;
+            }
+            else
+            {
+                bookListbox.DataSource = books
+                    .Where(b => b.Genre == currentGenre)
+                    .ToList();
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = checkedListBox1.SelectedItem as string;
+            label1.Text = item;
+            var hasExist = checkedListBox1.CheckedItems.Contains(item);
+            if (hasExist)
+            {
+                label1.BackColor = Color.SpringGreen;
+            }
+            else
+            {
+                label1.BackColor = Color.White;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add(textBox1.Text);
+        }
+        
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            var list = checkedListBox1.Items;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var hasExists = checkedListBox1.CheckedItems.Contains(list[i]);
+                if (hasExists)
+                {
+                    checkedListBox1.Items.Remove(list[i]);
+                    --i;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text=comboBox1.SelectedItem.ToString();
+        }
+    }
+}
+
